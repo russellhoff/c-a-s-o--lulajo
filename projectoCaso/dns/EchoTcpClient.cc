f300dca857be13fa9581ceee14f91a1aa922e0b9
davidlallana@gmail.com <davidlallana@gmail.com@c8412a99-2c33-ecc0-6c8c-1e9d762132fc>
2013-03-06 17:53:45 +0000

    gghf

--- projectoCaso/dns/EchoTcpClient.cc
+++ projectoCaso/dns/EchoTcpClient.cc
@@ -3,8 +3,42 @@
 
 #include "TcpListener.h"
 
-int main() {
-/*
+void usage() {
+	cout << "Usage: EchoTcpClient <name-server-port> <echo-server-domain-name-to-resolve>" << endl;
+	exit(1);
+}
+
+int main(int argc, char** argv) {
+
+	if (argc != 3) {
+		usage();
+	}
+
+	PracticaCaso::TcpClient * client = new PracticaCaso::TcpClient();
+	client->connect("127.0.0.1", atoi(argv[1]));
+	string dnsName = argv[2];
+	client->send(dnsName);
+	string ipAddressAndPort = client->receive();
+	client->close();
+
+	if (ipAddressAndPort.find("ERROR") == 0) {
+		cout << "The DNS name " << dnsName << " could not be resolved." << endl;
+	} else {
+		cout << "The DNS name: " << dnsName << " corresponds to: " << ipAddressAndPort << endl;
+		client->connect(ipAddressAndPort.substr(0, ipAddressAndPort.find(":")), atoi((ipAddressAndPort.substr(ipAddressAndPort.find(":")+1)).c_str()));
+		string msg = "Hello CASO students!";
+		client->send(msg);
+		cout << "Message sent: " << msg << endl;
+		msg = client->receive();
+		cout << "Message received: " << msg << endl;
+		client->close();
+	}
+
+	delete client;
+}
+
+/* int main() {
+
 	// MODIFICATION 2.3.3
 		PracticaCaso::TcpClient * client = new PracticaCaso::TcpClient();
 		client->connect("127.0.0.1", 4321);
@@ -15,7 +49,7 @@ int main() {
 		cout << "Message received: " << msg << endl;
 		client->close();	
 		delete client;
-*/
+
 	// MODIFICATION 2.3.5
 	for (int i=0; i<100; i++) {
 		PracticaCaso::TcpClient * client = new PracticaCaso::TcpClient();
@@ -29,5 +63,5 @@ int main() {
 		client->close();	
 		delete client;
 	}
-}
+}*/
 
