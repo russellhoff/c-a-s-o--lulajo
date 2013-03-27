2b5f460b8ea8f1889abe288564d018099a02f149
davidlallana@gmail.com <davidlallana@gmail.com@c8412a99-2c33-ecc0-6c8c-1e9d762132fc>
2013-03-27 09:18:22 +0000

    BORRO BRANCH

--- projectoCasoPpalBbdd/util/KeyValueDB.sql
+++
@@ -1,7 +0,0 @@
-create table KeyValuePair
-(
-  key_element      BLOB NOT NULL PRIMARY KEY,
-  value_element    BLOB
-);
-
--- insert into KeyValuePair(key_element, value_element) values ('deusto.es',  '127.0.0.1:3000');
